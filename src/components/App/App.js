17a9d607a33b632c3a257f7bb17d9f4a2d24034d
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-04-10 12:32:35 -0400

    Create function to show all tricks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,36 @@
 import { Component } from 'react';
 import './App.css';
+import {fetchTricks} from '../../apiCalls'
+import TrickContainer from '../trickContainer/TrickContainer';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tricks: [],
+      error: ""
+    }
+  }
+
+  componentDidMount(){
+    fetchTricks()
+    .catch((error) => {
+      console.error(error.message)
+      this.setState({error: error.message})
+    })
+    .then((data) => {
+      console.log('data', data)
+      this.setState({tricks: data})
+     
+    })
+  }
   render() {
+    console.log(this.state.tricks)
     return (
       <div className="App">
+        <h1>{console.log('PROPS', this.state.tricks)}</h1>
         <h1>Sick Trick Wish List</h1>
+       <TrickContainer tricks={this.state.tricks} />
       </div>
     );
   }
---
+++ src/components/trickContainer/TrickContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TrickContainer.css'
+
+const TrickContainer = props => {
+    console.log('props', props)
+    const newTricks = props.tricks.map(tricks => {
+        return (
+            <div className="tricks">
+                <h3>{tricks.name}</h3>
+                <p>{tricks.stance}</p>
+            </div>
+        )
+    })
+    console.log('newTricks', newTricks)
+    return(
+        <div className='trick-container'>
+            {newTricks}
+        </div>
+    )
+}
+
+
+export default TrickContainer
